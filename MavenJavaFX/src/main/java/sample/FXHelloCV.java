27a0ec29166adc39be60dd6d18535b8af60de76a
black45 <33381841+black45@users.noreply.github.com>
2020-02-20 08:42:31 +0000

    Add files via upload

--- MavenJavaFX/src/main/java/sample/FXHelloCV.java
+++ MavenJavaFX/src/main/java/sample/FXHelloCV.java
@@ -18,25 +18,18 @@ public class FXHelloCV extends Application{
     {
         try
         {
-            // load the FXML resource
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/sample.fxml"));
             BorderPane root = (BorderPane) loader.load();
-            // set a whitesmoke background
             root.setStyle("-fx-background-color: whitesmoke;");
-            // create and style a scene
             Scene scene = new Scene(root, 800, 600);
-            // create the stage with the given title and the previously created
-            // scene
             primaryStage.setTitle("Face Detection and Tracking");
             primaryStage.setScene(scene);
-            // show the GUI
             primaryStage.show();
 
-            // init the controller
+
             FXHelloCVController controller = loader.getController();
             controller.init();
 
-            // set the proper behavior on closing the application
             primaryStage.setOnCloseRequest((new EventHandler<WindowEvent>() {
                 public void handle(WindowEvent we)
                 {
@@ -49,12 +42,7 @@ public class FXHelloCV extends Application{
             e.printStackTrace();
         }
     }
-    /**
-     * For launching the application...
-     *
-     * @param args
-     *            optional params
-     */
+
     public static void main(String[] args)
     {
         nu.pattern.OpenCV.loadShared();
